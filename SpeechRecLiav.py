7e52bd274990bc319b182ed20f44564e3dcc4415
liavr6 <56167356+liavr6@users.noreply.github.com>
2019-10-09 22:35:45 +0300

    Add files via upload

---
+++ SpeechRecLiav.py
@@ -0,0 +1,51 @@
+import random
+import time
+import speech_recognition as sr
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+if __name__ == "__main__":
+
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    while (True):
+        guess = recognize_speech_from_mic(recognizer, microphone)
+        if guess["transcription"]:
+            break
+        if not guess["success"]:
+            break
+        print("I didn't catch that. What did you say?\n")
+
+        # show the user the transcription
+        print("{}".format(guess["transcription"]))
